945959c1548ef0adb3831ab49b09d9bc00b3871f
robinlyang <robiny@gmail.com>
2016-11-09 18:58:35 -0700

    Initial Project

--- NetTWO_ArrayList/Program.cs
+++ NetTWO_ArrayList/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace NetTWO_ArrayList
 {
@@ -10,6 +11,26 @@ namespace NetTWO_ArrayList
     {
         static void Main(string[] args)
         {
+            int[] i = new int[3];
+            Console.WriteLine(i.Length);
+
+            ArrayList a = new ArrayList(); //you can allocat a number if you with ... = new ArrayList(1000);
+            a.Add(200);
+            a.Add(100);
+            a.Add(400);
+
+            a.Sort();
+
+            int total = 0;
+
+            for (int x = 0; x < a.Count; x++)
+            {
+                total = total + (int)a[x]; // must cast collections to use them total = total + a[x]; won't work
+                Console.WriteLine(a[x]);
+            }
+            Console.WriteLine(total);
+
+            Console.ReadLine();
         }
     }
 }
