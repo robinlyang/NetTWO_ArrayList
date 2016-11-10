7e70f1d94832572a17515dd21ea8accf6780b48a
robinlyang <robiny@gmail.com>
2016-11-09 19:49:06 -0700

    SortedList, Stack, and Queue examples

--- NetTWO_ArrayList/Program.cs
+++ NetTWO_ArrayList/Program.cs
@@ -30,6 +30,32 @@ namespace NetTWO_ArrayList
             }
             Console.WriteLine(total);
 
+            //SortedLists
+            SortedList s = new SortedList();
+            //key can be anything BUT first key determines type
+            s[1234567] = "bob"; // adds if not found (1234567 is the key)
+            s[1234567] = "ed"; //replace
+            s[7654321] = "mary";
+            s[123] = "ted";
+            //s["AB"] = "ted"; will give an error
+            if(s.ContainsKey(123)) //ContainsKey checks if data is already there
+            {
+                s.Remove(123); //will remove this data from the sorted list
+            }
+            //Stack
+            Stack ss = new Stack(); //list in first out LIFO
+            ss.Push(100);
+            ss.Push(200);
+            ss.Push(400);
+            Console.WriteLine(ss.Pop()); //shows and removes from the top of the stack (i.e. 400)
+            Console.WriteLine(ss.Peek()); //shows the next available number (only one level down)
+            //Queue
+            Queue qq = new Queue(); //first in first out FIFO
+            qq.Enqueue(100);
+            qq.Enqueue(200);
+            qq.Enqueue(400);
+            Console.WriteLine(qq.Dequeue());//shows and removes first one entered into queue (i.e. 100)
+
             Console.ReadLine();
         }
     }
